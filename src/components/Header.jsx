38f968afd1d3239727de5dc7440aba224264493f
WitcherxSN <shravan.naik30@gmail.com>
2026-08-09 01:01:45 +0530

    shoppyglobe is getting ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+    const cartItems = useSelector((state) => state.cart.items);
+
+const totalItems = cartItems.reduce(
+  (total, item) => total + item.quantity,
+  0
+);
   return (
     <header>
       <h2>ShoppyGlobe</h2>
 
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/cart">🛒 Cart</Link>
+        <Link to="/cart"> 🛒 Cart ({totalItems})</Link>
         <Link to="/checkout">Checkout</Link>
       </nav>
     </header>
